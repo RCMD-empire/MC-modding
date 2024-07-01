a4d73f080678f3040486b1fdb375d01c1d760a63
LABOR210-25\hallgato <>
2024-07-01 11:20:20 +0200

    vars

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("heeyy!");
+        String firstname = "Áron";
+        String lastname = "Falus";
+        System.out.println(firstname+" "+lastname);
     }
 }
