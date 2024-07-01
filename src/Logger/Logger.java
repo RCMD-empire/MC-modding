ffc3bebec1f8e6519a32f494447695e80549be35
LABOR210-25\hallgato <>
2024-07-01 13:49:43 +0200

    LogLevel (functional)

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -4,47 +4,61 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
-    public static final String RESET = "\u001B[0m";
-    public static final String YELLOW = "\u001B[0m";
-    public static final String RED = "\u001B[0m";
-    static enum Level { DEBUG, INFO, WARN, ERROR }
-    public String Name;
+    private static final String RESET = "\u001B[0m";
+    private static final String YELLOW = "\u001B[33m";
+    private static final String RED = "\u001B[31m";
+    public static enum Level { DEBUG, INFO, WARN, ERROR }
+    private String Name;
+    private Level GlobalLogLevel;
 
-    public Logger(String name) {
+    public Level getGlobalLogLevel() {
+        return GlobalLogLevel;
+    }
+
+    public void setGlobalLogLevel(Level globalLogLevel) {
+        GlobalLogLevel = globalLogLevel;
+    }
+    // 2 params constructor
+    public Logger(String name, Level globalLogLevel) {
         Name = name;
+        GlobalLogLevel=globalLogLevel;
+    }
+    // 1 param constructor
+    public Logger(String name) {
+        this(name,Level.DEBUG);
     }
 
     private void Output(Level lvl, String msg) {
-        String out="";
-        // coloring
-        switch (lvl)
-        {
-            case DEBUG:
-                out = RESET;
-                break;
-            case INFO:
-                out=RESET;
-                break;
-            case WARN:
-                out=YELLOW;
-                break;
-            case ERROR:
-                out=RED;
-                break;
+        if (GlobalLogLevel.ordinal() <= lvl.ordinal()) {
+            String out="";
+            // coloring
+            switch (lvl)
+            {
+                case DEBUG:
+                    //out = RESET;
+                    break;
+                case INFO:
+                    //out = RESET;
+                    break;
+                case WARN:
+                    out = YELLOW;
+                    break;
+                case ERROR:
+                    out = RED;
+                    break;
+            }
+            //Get current date time
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formatDateTime = now.format(formatter);
+            // formatting output
+            out += "[" + formatDateTime + "] [" + lvl + "] [" + Name + "] " + msg;
+
+            // reverse coloring
+            out += RESET;
+            // writing out
+            System.out.println(out);
         }
-//        level.ordinal() <= lvl.ordinal()
-        //if(lvl.ordinal())
-        //Get current date time
-        LocalDateTime now = LocalDateTime.now();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-
-        String formatDateTime = now.format(formatter);
-
-        out+= "["+formatDateTime+"] ["+lvl+"] ["+Name+"] "+msg;
-        out+=RESET;
-        System.out.println(out);
-
     }
     public void debug(String msg) {
         Output(Level.DEBUG,msg);
@@ -57,5 +71,13 @@ public class Logger {
     }
     public void error(String msg) {
         Output(Level.ERROR,msg);
+    }
+    public static void GlobalLogLevelTester(Level lvl){
+        Logger log = new Logger("GlobalLogLevelTester");
+        log.setGlobalLogLevel(lvl);
+        log.debug("Debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("Error");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import Logger.Logger;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class Main {
     public static void main(String[] args) {
-        Logger log = new Logger("Test");
-        log.debug("asd");
+        Logger.GlobalLogLevelTester(Logger.Level.DEBUG);
     }
 }
