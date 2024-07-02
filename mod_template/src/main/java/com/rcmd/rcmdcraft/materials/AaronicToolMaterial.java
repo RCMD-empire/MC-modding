6ccd1f7113466262e1a3d6d4758de1e55372c003
rcmdkick <>
2024-07-02 14:39:16 +0200

    had to go, having syntax problem at ..ToolMaterial.java:8-9 row

---
+++ mod_template/src/main/java/com/rcmd/rcmdcraft/materials/AaronicToolMaterial.java
@@ -0,0 +1,39 @@
+package com.rcmd.rcmdcraft.materials;
+
+import com.rcmd.rcmdcraft.items.ModItems;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public enum AaronicToolMaterial implements ToolMaterial {
+    MATERIAL(4,3600,12.0F,4.0F,10,
+            ()->{return Ingredient.ofItems(ModItems.AARE);});
+    @Override
+    public int getDurability() {
+        return 0;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 0;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 0;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return null;
+    }
+}
