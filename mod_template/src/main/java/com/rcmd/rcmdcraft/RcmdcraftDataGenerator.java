cf8e9c6c6a3fc4de29278c5ed8b5fd7f2e31f401
rcmdkick <>
2024-07-02 10:29:00 +0200

    unzip and open it as a project.
    Then File -> Settings -> "Build, Executetion, Deployment" -> gradle: change java version to 17 (should be installed before).
    
    Then File -> Project Structure -> change the SDK to 17 too.
    
    Then "add application" and select minecraft lient and OK.

---
+++ mod_template/src/main/java/com/rcmd/rcmdcraft/RcmdcraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.rcmd.rcmdcraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RcmdcraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
