9ab9f96075d287c730abd336b4fac79ee0d38067
rcmdkick <>
2024-07-02 11:16:26 +0200

    Registrering (import namespaces important)

--- mod_template/src/main/java/com/rcmd/rcmdcraft/Rcmdcraft.java
+++ mod_template/src/main/java/com/rcmd/rcmdcraft/Rcmdcraft.java
@@ -4,6 +4,8 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +23,7 @@ public class Rcmdcraft implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		Registry.register(Registry.ITEM,new Identifier(ModID, "aare"),AARE);
 		LOGGER.info(ModID +" has loaded!");
 	}
 }
