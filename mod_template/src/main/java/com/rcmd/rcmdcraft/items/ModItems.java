fdd19ee8070a430a6e0bbdbb2b3faab290f9e95e
rcmdkick <>
2024-07-03 13:22:08 +0200

    Furnace use: Smelting ("Smelted Aare")

--- mod_template/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
+++ mod_template/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
@@ -10,7 +10,10 @@ import static com.rcmd.rcmdcraft.Rcmdcraft.ModID;
 
 public class ModItems {
     public static final Item AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
+    public static final Item SMELTED_AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
+
     public static void RegisterModItems(){
         Registry.register(Registry.ITEM,new Identifier(ModID, "aare"),AARE);
+        Registry.register(Registry.ITEM,new Identifier(ModID, "smelted_aare"),SMELTED_AARE);
     }
 }
