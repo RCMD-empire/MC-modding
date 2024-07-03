91f2b4b917ab8c70e0d0a67c7184f7bf71c69933
rcmdkick <>
2024-07-03 14:02:06 +0200

    Pickaxe ("Aaronic_pickaxe")

--- mod_template/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
+++ mod_template/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
@@ -1,8 +1,10 @@
 package com.rcmd.rcmdcraft.items;
 
 import com.rcmd.rcmdcraft.itemgroups.ModItemGroups;
+import com.rcmd.rcmdcraft.materials.AaronicToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,8 +14,11 @@ public class ModItems {
     public static final Item AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
     public static final Item SMELTED_AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
 
+    public static final PickaxeItem AARONIC_PICKAXE = new PickaxeItem(AaronicToolMaterial.MATERIAL,1,1,new Item.Settings().group(ModItemGroups.RCMD_GROUP));
+
     public static void RegisterModItems(){
         Registry.register(Registry.ITEM,new Identifier(ModID, "aare"),AARE);
         Registry.register(Registry.ITEM,new Identifier(ModID, "smelted_aare"),SMELTED_AARE);
+        Registry.register(Registry.ITEM,new Identifier(ModID, "aaronic_pickaxe"),AARONIC_PICKAXE);
     }
 }
