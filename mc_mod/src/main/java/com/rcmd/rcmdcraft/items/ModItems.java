c69bf3455c5be84fbc7b6a803bb250aed478c90e
rcmdkick <>
2024-07-04 12:45:22 +0200

    "aaronic" item implemented, (and some typo)

--- mc_mod/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
+++ mc_mod/src/main/java/com/rcmd/rcmdcraft/items/ModItems.java
@@ -12,12 +12,14 @@ import static com.rcmd.rcmdcraft.Rcmdcraft.ModID;
 
 public class ModItems {
     public static final Item AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
+    public static final Item AARONIC = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
     public static final Item SMELTED_AARE = new Item(new Item.Settings().group(ModItemGroups.RCMD_GROUP));
 
     public static final PickaxeItem AARONIC_PICKAXE = new PickaxeItem(AaronicToolMaterial.MATERIAL,1,1,new Item.Settings().group(ModItemGroups.RCMD_GROUP));
 
     public static void RegisterModItems(){
         Registry.register(Registry.ITEM,new Identifier(ModID, "aare"),AARE);
+        Registry.register(Registry.ITEM,new Identifier(ModID, "aaronic"),AARONIC);
         Registry.register(Registry.ITEM,new Identifier(ModID, "smelted_aare"),SMELTED_AARE);
         Registry.register(Registry.ITEM,new Identifier(ModID, "aaronic_pickaxe"),AARONIC_PICKAXE);
     }
