88b5210a544e0c54c2a901200bccfd4cfad6f1b1
rcmdkick <falusaron.mano@gmail.com>
2024-07-26 19:02:40 +0200

    inventory position fix

--- mc_mod/src/main/java/com/rcmd/rcmdcraft/screen/IonizatorScreenHandler.java
+++ mc_mod/src/main/java/com/rcmd/rcmdcraft/screen/IonizatorScreenHandler.java
@@ -29,8 +29,8 @@ public class IonizatorScreenHandler extends ScreenHandler {
         inventory.onOpen(playerInventory.player);
         this.propertyDelegate = delegate;
         // (menu) picture dependent
-        this.addSlot(new Slot(inventory, 0, 12, 15));
-        this.addSlot(new Slot(inventory, 1, 86, 15));
+        this.addSlot(new Slot(inventory, 0, 80, 11));
+        this.addSlot(new Slot(inventory, 1, 80, 59));
 
         addPlayerInventory(playerInventory);
         addPlayerHotbar(playerInventory);
@@ -69,14 +69,14 @@ public class IonizatorScreenHandler extends ScreenHandler {
     private void addPlayerInventory(PlayerInventory playerInventory) {
         for (int i = 0; i < 3; ++i) {
             for (int l = 0; l < 9; ++l) {
-                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
+                this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 84 + i * 18));
             }
         }
     }
 
     private void addPlayerHotbar(PlayerInventory playerInventory) {
         for (int i = 0; i < 9; ++i) {
-            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
+            this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 142));
         }
     }
     public boolean isCrafting()
